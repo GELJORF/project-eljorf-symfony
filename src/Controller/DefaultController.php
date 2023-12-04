793f58d1c3a6de4db070500326d72e626c07c361
unknown <toto@t.fr>
2023-12-04 17:27:07 +0100

    pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,9 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig', [
+            'menu' => $this->getMenu(),
+        ]);
+}
+    private function getMenu(): array
+    {
+        return [
+            'Accueil' => $this->generateUrl('homepage'),
+            'À propos' => $this->generateUrl('a_propos'),
+            'Cours' => $this->generateUrl('cours'),
+            'Newsletter' => $this->generateUrl('newsletter'),
+            'Contact' => $this->generateUrl('contact'),
+            'Inscription' => $this->generateUrl('inscription'),
+            'Connexion' => $this->generateUrl('connexion'),
+        ];
     }
 }
