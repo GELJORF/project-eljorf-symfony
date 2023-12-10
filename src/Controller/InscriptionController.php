2f9a7868934edea4229542d998716bd41756cc96
unknown <toto@t.fr>
2023-12-10 21:09:42 +0100

    Registration

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InscriptionController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Inscription réussie, vous pouvez désormais vous connecter.');
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
